385bc887aa872b7c69271504194f5767e157d65b
Philan-igmt <philanisithembiso@gmail.com>
2020-07-10 23:26:53 +0200

    Updates

---
+++ precache-manifest.ecdfcbc854deae9e380f9ba730cd5ef6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c872126a3e0c3450ecee57a655c3faa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "801b7670bc5abd8ef758",
+    "url": "/static/css/2.0c041d17.chunk.css"
+  },
+  {
+    "revision": "6f555623bdd0ccc313ca",
+    "url": "/static/css/main.b9bd8ac6.chunk.css"
+  },
+  {
+    "revision": "801b7670bc5abd8ef758",
+    "url": "/static/js/2.51aea5bc.chunk.js"
+  },
+  {
+    "revision": "109083572dc3ba74f105a9f6f5c9e305",
+    "url": "/static/js/2.51aea5bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f555623bdd0ccc313ca",
+    "url": "/static/js/main.c6085f21.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "679a104b7095cc55d6f16cdad67ea55f",
+    "url": "/static/media/Godwin.679a104b.jpg"
+  },
+  {
+    "revision": "1e2a5718efc937170dfc40ecc7c4c170",
+    "url": "/static/media/LifeChoices.1e2a5718.jpg"
+  },
+  {
+    "revision": "96894b246dce26c23ced092edfabf34b",
+    "url": "/static/media/coding.96894b24.jpg"
+  },
+  {
+    "revision": "6e30121f94fad1f94a42f962c343d664",
+    "url": "/static/media/cv.6e30121f.docx"
+  },
+  {
+    "revision": "029c5532764f7b613d7a47119bc669c0",
+    "url": "/static/media/ganief.029c5532.jpg"
+  },
+  {
+    "revision": "b46d0829f0d9ed22aecdec712c3d578e",
+    "url": "/static/media/jason.b46d0829.jpeg"
+  },
+  {
+    "revision": "0f1d3b1688e7a779292bad2fd651eb5b",
+    "url": "/static/media/philani.0f1d3b16.png"
+  },
+  {
+    "revision": "ed8ea805d35d98e235893c5ac7a9f4c6",
+    "url": "/static/media/yonela.ed8ea805.jpeg"
+  }
+]);
