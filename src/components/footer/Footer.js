a08db7ab73bf18683eede236471787ee94f51cb9
Philan-igmt <philanisithembiso@gmail.com>
2020-07-17 07:12:38 +0200

    footer changes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="footer">
       <div className="content">
         <h5 className="foo">&copy;2020 By Philan ISithembiso</h5>
-        <h5>Sponsored by</h5>
+        <h5>Supported by</h5>
         <span>
           <img className="image" src={Logo} alt="" />
         </span>
